8ecb7cb5ccb9865daed5fed0b76255825f5fdb09
Jake Reich <mdj69xbl@gmail.com>
2022-02-21 17:02:13 +0000

    added tuples, code updated

--- mpags_eppt_jake_reich.py
+++ mpags_eppt_jake_reich.py
@@ -136,89 +136,52 @@ def zx_track_reco(df):
     DC1_x = np.delete(DC1_x, remove_events)
     DC2_x = np.delete(DC2_x, remove_events)
 
-
-    # zx_tracks_DC1 = {}
-    # zx_tracks_DC2 = {}
-    # n_multiple_hits = 0
-
-    # for ev in range(0,len(DC1_z)): # loop through events
-    #
-    #     # store number of multiple hits in event
-    #     DC1_N_multiple_hits = 0
-    #     DC2_N_multiple_hits = 0
-    #     DC1_multiple = {}
-
-
-
-        # for z in range(0,len(DC1_z[ev])):
-        #     n_multiple_hits = list(DC1_z[ev]).count(DC1_z[ev][z]) # number of multiple hits for DC1 chamber z
-        #
-        #     if(n_multiple_hits > 2): # duplicate hit in zth DC1
-        #         print(DC1_z[ev])
-        #         zipped_ev = zip(DC1_z[ev], DC1_x[ev])
-        #         for L in range(0, len(DC1_z[ev])+1):
-        #             for subset in itertools.combinations(zipped_ev[0], L):
-        #                 if(len(subset) == len(DC1_z[ev])-(n_multiple_hits-1)):
-        #                     print(subset)
-        #         sys.exit()
-
-
-            #if(DC1_z[ev][z] in np.delete(DC1_z[ev], z)): # check that this drift chamber has more than 1 hit
-            #
-            # n_multiple_hits = DC1_z[ev].count(DC1_z[ev][z]) # number of multiple hits for DC1 chamber z
-            # if(n_multiple_hits > 1): # duplicate hit in zth DC1
-            #     for z_multi in range(0,len(DC1_z[ev])):
-            #         if(z_multi == z)
-
-                #
-                # DC1_multiple[str(DC1_z[ev][z])]
-
-
-
-        #
-        #     zx_tracks_DC1["eventNum_" + str(ev) ]["traj"] = [DC1_z[ev],DC1_x[ev]]
-        # zx_tracks_DC2["eventNum_" + str(ev) ] = [DC2_z[ev],DC2_x[ev]]
-
-
-
-
-
     print("Number of events with more than one hit in a drift chamber = " + str(n_multiple_hits))
 
     return DC1_x, DC2_x
 
 
-def plot_trajectories(zx_tracks, c,m, DC1 = True):
+def plot_trajectories(zx_tracks,  DC1 = True):
 
 
-    z_range = np.arange(0,len(zx_tracks)*0.5,0.5)
 
+
+    df = load_df(cwd, "/B5" + suffix)
+    zx_tracks_DC1, zx_tracks_DC2 = zx_track_reco(df)
+
+    if(DC1 == True):
+        zx_tracks = zx_tracks_DC1
+    else:
+        zx_tracks = zx_tracks_DC2
 
     fig_zx = plt.figure()
 
-    plt.scatter( z_range , zx_tracks, marker='x' )
-    # define trajectory fit's x and z ranges
-    z_fit = np.linspace(min(z_range),max(z_range), 1000)
-    x_fit = np.linspace(min(zx_tracks),max(zx_tracks),1000)
 
-    plt.plot(z_fit, c + m*z_fit, color = 'red')
+    for ev in range(0,len(zx_tracks)):
+        z_range = np.arange(0,len(zx_tracks[ev])*0.5,0.5)
+        plt.plot( z_range , zx_tracks[ev])
+        # define trajectory fit's x and z ranges
+        # z_fit = np.linspace(min(z_range),max(z_range), 1000)
+        # x_fit = np.linspace(min(zx_tracks),max(zx_tracks),1000)
+        #
+        # plt.plot(z_fit, c + m*z_fit, color = 'red')
 
 
     plt.xlabel("z [m]")
     plt.ylabel("x [m]")
 
     if(DC1 == True):
-        plt.vlines(z_range, -100, 100)
+        # plt.vlines(z_range, -100, 100)
         plt.savefig("DC1_zx"+suffix+".pdf")
     else:
-        plt.vlines(z_range, -100, 100)
+        # plt.vlines(z_range, -100, 100)
         plt.savefig("DC2_zx"+suffix+".pdf")
 
 
-def plot_momentum_resolution(momenta, B, l):
+def plot_momentum_resolution(momenta,m_1_arr,m_2_arr, B, l):
     sigma_x = 100e-6 # x precision
-    h = 5*0.5 # length of arm
-    p_res =  (sigma_x/h)*(momenta/(0.3*B*l))
+    h = 1 + (np.array(m_1_arr)*np.array(m_2_arr)) # denominator of tan(theta)
+    p_res =  (sigma_x/h)*(np.array(momenta)/(0.3*B*l))
 
 
     fig = plt.figure(figsize = (8,6))
@@ -228,20 +191,20 @@ def plot_momentum_resolution(momenta, B, l):
     bin_centres = (bin_edges[:-1] + bin_edges[1:])/2
 
     p_res_range = np.linspace(min(p_res), max(p_res), 1000)
-    popt, pcov = curve_fit(gauss, bin_centres, data, p0 = [1,1,1])
+    popt, pcov = curve_fit(gauss, bin_centres, data, p0 = [160,0.01,0.001])
 
     plt.plot(p_res_range, gauss(p_res_range, *popt), color = 'red', label = 'Gaussian Fit')
     plt.legend(fontsize = f_size)
     plt.ylabel('Number of Events')
 
     textstr = '\n'.join((
-    r'$A=%.3f \pm %.3f$' % (popt[0], np.sqrt(pcov[0][0]) ),
-    r'$\mu=%.3f \pm %.3f$' % (popt[1],np.sqrt(pcov[1][1]) ),
-    r'$\sigma=%.3f \pm %.3f$' % (abs(popt[2]),np.sqrt(pcov[2][2]) )))
+    r'$A=%.5f \pm %.5f$' % (popt[0], np.sqrt(pcov[0][0]) ),
+    r'$\mu=%.5f \pm %.5f$' % (popt[1],np.sqrt(pcov[1][1]) ),
+    r'$\sigma=%.5f \pm %.5f$' % (abs(popt[2]),np.sqrt(pcov[2][2]) )))
 
     props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 
-    plt.text(max(bin_edges)*0.85, max(data)*0.8, textstr, fontsize=f_size,
+    plt.text(max(bin_edges)*0.91, max(data)*0.8, textstr, fontsize=f_size,
             verticalalignment='top', bbox=props)
 
 
@@ -272,7 +235,7 @@ def plot_momenta(momenta):
 
     momemta_range = np.linspace(min(momenta), max(momenta), 1000)
     if("pb" not in suffix):
-        popt, pcov = curve_fit(gauss, bin_centres, data, p0 = [1,50,10])
+        popt, pcov = curve_fit(gauss, bin_centres, data, p0 = [1,200,10])
         textstr = '\n'.join((
         r'$A=%.3f \pm %.3f$' % (popt[0], np.sqrt(pcov[0][0]) ),
         r'$\mu=%.3f \pm %.3f$' % (popt[1],np.sqrt(pcov[1][1]) ),
@@ -286,6 +249,7 @@ def plot_momenta(momenta):
         r'$\omega=%.3f \pm %.3f$' % (popt[2], np.sqrt(pcov[2][2]) ),
         r'$\xi=%.3f \pm %.3f$' % (popt[3], np.sqrt(pcov[3][3]) )))
         plt.plot(momemta_range, skewNorm(momemta_range, *popt), color = 'red', label = 'Skew Normal Fit')
+        plt.xlim(0,250)
     plt.legend(fontsize = f_size)
     plt.xlabel(r'momentum $[GeV]$')
     plt.ylabel('Number of Events')
@@ -294,46 +258,80 @@ def plot_momenta(momenta):
 
     props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 
-    plt.text(max(bin_edges)*0.7, max(data)*0.8, textstr, fontsize=f_size,
+    plt.text(max(bin_edges)*0.35, max(data)*0.8, textstr, fontsize=f_size,
             verticalalignment='top', bbox=props)
 
     plt.savefig("p_dist"+ suffix +".pdf")
 
     return
 
+def getCalorimeterEnergy(df):
+
+    ECalEnergy = df["ECEnergy"].to_numpy()/1000
+    HCalEnergy = df["HCEnergy"].to_numpy()/1000
+    ECalEnergy_vec = df["ECEnergyVector"].to_numpy()/1000
+    HCalEnergy_vec = df["HCEnergyVector"].to_numpy()/1000
+
+    return ECalEnergy,HCalEnergy,ECalEnergy_vec,HCalEnergy_vec
+
+
+def plotCalDists(ECal, HCal, ECal_vec, HCal_vec):
+    range = np.linspace(0, 1, nbins)
+    fig = plt.figure()
+    plt.hist(ECal, label = "ECal",bins =  range, histtype = "step", color = "blue")
+    #plt.hist(HCal, label = "HCal", bins =  range, histtype = "step", color = "orange")
+    plt.legend()
+    # plt.xlim(0,0.6)
+    plt.xlabel(r'Total Energy Deposited [GeV]')
+    plt.ylabel('Number of Events')
+    plt.savefig("calorimeter_"+ suffix +"_ecal_.pdf")
+
 
 if __name__ == "__main__":
-    nbins = 30
+    nbins = 50
     f_size = 13
 
-    suffix = "_05T_100P_pbBeforeMag"
+    suffix = "_05T_100P"
     df = load_df(cwd, "/B5" + suffix)
 
+    # zx_tracks_DC1, zx_tracks_DC2 = zx_track_reco(df)
+    # momenta = []
+    # chi_square_DC1 = []
+    # chi_square_DC2 = []
+    # m_1_arr = []
+    # m_2_arr = []
+    # for ev in range(0,len(zx_tracks_DC1)):
+    #     # extract hits from event
+    #     hits_dc1 = zx_tracks_DC1[ev]
+    #     hits_dc2 = zx_tracks_DC2[ev]
+    #
+    #     # fit
+    #     c_1, m_1, chisq_1 = fit_trajectory(np.arange(0,len(hits_dc1)*0.5,0.5), hits_dc1 )
+    #     c_2, m_2, chisq_2 = fit_trajectory(np.arange(0,len(hits_dc2)*0.5,0.5), hits_dc2 )
+    #
+    #     m_1_arr.append(m_1)
+    #     m_2_arr.append(m_2)
+    #
+    #
+    #
+    #
+    #     # get momentum
+    #     momenta.append(calc_momentum(c_1, m_1, c_2, m_2, 0.5, 2))
+    #
+    #     # store chisquare
+    #     chi_square_DC1.append(chisq_1)
+    #     chi_square_DC2.append(chisq_2)
+    #
+    # plot_trajectories(hits_dc1, DC1 = True)
+    # plot_trajectories(hits_dc2, DC1 = False)
+    #
+    # momenta = np.array(momenta)
+    # plot_momenta(momenta)
+    # # plot_chisquare(chi_square_DC1,chi_square_DC2)
+    # plot_momentum_resolution(momenta,m_1_arr,m_2_arr, 0.5 ,2)
 
-    zx_tracks_DC1, zx_tracks_DC2 = zx_track_reco(df)
-    momenta = []
-    chi_square_DC1 = []
-    chi_square_DC2 = []
-    for ev in range(0,len(zx_tracks_DC1)):
-        # extract hits from event
-        hits_dc1 = zx_tracks_DC1[ev]
-        hits_dc2 = zx_tracks_DC2[ev]
 
-        # fit
-        c_1, m_1, chisq_1 = fit_trajectory(np.arange(0,len(hits_dc1)*0.5,0.5), hits_dc1 )
-        c_2, m_2, chisq_2 = fit_trajectory(np.arange(0,len(hits_dc2)*0.5,0.5), hits_dc2 )
 
-        # plot_trajectories(hits_dc1, c_1,m_1, DC1 = True)
-        # plot_trajectories(hits_dc2, c_2,m_2, DC1 = False)
-
-        # get momentum
-        momenta.append(calc_momentum(c_1, m_1, c_2, m_2, 0.5, 2))
-
-        # store chisquare
-        chi_square_DC1.append(chisq_1)
-        chi_square_DC2.append(chisq_2)
-
-    momenta = np.array(momenta)
-    plot_momenta(momenta)
-    # plot_chisquare(chi_square_DC1,chi_square_DC2)
-    # plot_momentum_resolution(momenta, 0.25 ,2)
+    ECalEnergy,HCalEnergy,ECalEnergy_vec,HCalEnergy_vec = getCalorimeterEnergy(df)
+    plotCalDists(ECalEnergy, HCalEnergy, ECalEnergy_vec,HCalEnergy_vec)
+    # for ev in range(0,len(ECalEnergy)):
